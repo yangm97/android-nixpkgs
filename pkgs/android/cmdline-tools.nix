80ebe8dc30dc0da0bd92585187b4bca75c828ae0
Yan Minari <yangm97@gmail.com>
2023-08-31 20:16:54 -0300

    fix: ensure $JAVA_HOME points to the jdk defined in the pkg

--- pkgs/android/cmdline-tools.nix
+++ pkgs/android/cmdline-tools.nix
@@ -7,7 +7,7 @@ mkGeneric
   passthru.installSdk = ''
     for script in $pkgBase/bin/*; do
       makeWrapper $script $out/bin/$(basename $script) \
-        --set-default JAVA_HOME "${jdk.home}" \
+        --set JAVA_HOME "${jdk.home}" \
         --set-default ANDROID_SDK_ROOT $ANDROID_SDK_ROOT \
         --prefix JAVA_OPTS ' ' "-Dcom.android.sdklib.toolsdir=$pkgBase" \
         --prefix JAVA_OPTS ' ' "-Dcom.android.sdkmanager.toolsdir=$pkgBase" \
